291da2de438ecfecc8ddfa6d3f0c5d9fe108497a
codedbyelif <elifkyr3@gmail.com>
2026-01-11 04:00:48 +0300

    feat : footer added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import HeroSection from "@/components/HeroSection";
 import FAQ from "@/components/FAQ";
+import { Footer7 } from "@/components/footer7";
 
 export default function Page() {
   return (
@@ -8,6 +9,7 @@ export default function Page() {
       <Hero />
       <HeroSection />
       <FAQ />
+      <Footer7 />
     </>
   );
 }
