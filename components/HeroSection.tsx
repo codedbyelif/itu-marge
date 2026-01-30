3fa78f2910e8c40ce0378e7d3f6221f24b2ba80b
codedbyelif <elifkyr3@gmail.com>
2026-01-30 22:32:26 +0300

    fix : The issue with the photo on the homepage has been resolved. (#11)

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
           <img
             alt="Card"
             className="pointer-events-none h-80 sm:h-96 lg:h-120 w-full object-cover"
-            src="https://instagram.fyei6-1.fna.fbcdn.net/v/t51.82787-15/574501635_17981888447916250_2127288403671688690_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=107&ig_cache_key=Mzc1OTgwODgzMDUyODgxNjE2MQ%3D%3D.3-ccb7-5&ccb=7-5&_nc_sid=58cdad&efg=eyJ2ZW5jb2RlX3RhZyI6InhwaWRzLjEyMDB4OTAwLnNkci5DMyJ9&_nc_ohc=S_EyZ-h4pjkQ7kNvwE5Gvzx&_nc_oc=AdmlOIAP561ggLYnCk2NnrjiLhzedgD3SWfQvBr2ZAWhewiIwDwJraG493yQ9y8xkuo&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=instagram.fyei6-1.fna&_nc_gid=uT_MMKYBacKzD7HqYpeXjA&oh=00_AfpVQW4AjcQnj64jDDrU64-J_kwdiqKBSeQ59TKmghV4Gg&oe=696DA0AE"
+            src="/itu-margetakimi.png"
           />
           <div className="pt-2 pb-1">
             <p className="text-lg font-medium tracking-tight text-background">
